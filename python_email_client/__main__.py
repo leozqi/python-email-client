09038cc7445b3e0d93a22b4398d7ed100d12ab96
Leo Qi <leozhaoqi@outlook.com>
2020-07-24 08:58:25 -0400

    Created base for threading for future parts

--- python_email_client/__main__.py
+++ python_email_client/__main__.py
@@ -1,136 +1,137 @@
 from multiprocessing import Pool
 from queue import Queue
 from functools import partial
-import sqlite3
 import tkinter as tk
+from tkinter import ttk
+import tkinter.messagebox
+import tkinter.simpledialog
 from email_conn import *
 from parser import *
+from database import *
+from threading import Thread
 
-VERSION = '0.0.1'
+VERSION = '0.0.2'
 
 class Application():     
     def __init__(self):
+        self.tasks = []
+        self.log = Queue()
+        # Arrange the basics of window
         self.root = tk.Tk()
-        self.root.geometry('800x600')
+        self.root.geometry('1000x750')
         self.root.title('PythonMail Client v.' + VERSION)
         self.root.iconbitmap('favicon.ico')
-        self.log = Queue()
-        
+        #self.root.protocol('WM_DELETE_WINDOW', self.close)
+        self.style = ttk.Style()
+        self.style.configure(
+            'Status.TLabel',
+            relief=tk.SUNKEN,
+            anchor=tk.W,
+        )
+        # Setup status bar
         self.status = tk.StringVar()
         self.status.set('Not connected')
-        self.statuslabel = tk.Label(
+        self.statuslabel = ttk.Label(
             self.root, textvariable=self.status,
-            bd=1, relief=tk.SUNKEN, anchor=tk.W)
+            style='Status.TLabel'
+        )
         self.statuslabel.pack(side=tk.BOTTOM, fill=tk.X)
 
-        # connect button
-        connectbutton = tk.Button(
-            self.root, text='Connect to server',
-            command=self.connect_mail)
-        connectbutton.pack()
+        # Setup menu bar
+        self.menubar = tk.Menu(self.root)
+        self.menubar.add_command(
+            label='Connect', 
+            command=self.connect
+        )
+        self.menubar.add_command(
+            label='Get emails for database', 
+            command=self.pre_get_mail
+        )
+        self.helpmenu = tk.Menu(self.menubar, tearoff=0)
+        self.helpmenu.add_command(
+            label='About', 
+            command=self.about
+        )
+        self.menubar.add_cascade(label='Help', menu=self.helpmenu)
+        self.root.config(menu=self.menubar)
+    
+        self.email_app = None # EmailConnection()
+        self.email_get = None # EmailGetter()
 
-        # download button
-        downloadmsgs = tk.Button(
-            self.root, text='Download Messages',
-            command=self.download_mail)
-        downloadmsgs.pack()
-        
-        self.EmailApp = None
-        self.update_status()
+        # Updater
+        self.tasks.append(Thread(target=self.update_status))
+        self.tasks[-1].setDaemon(True)
+        self.tasks[-1].start()
         self.root.mainloop()
     
+    """
+    def close(self):
+        for task in self.tasks:
+            task.cancel()
+        self.loop.stop()
+        self.root.destroy()
+    """
+
+    def about(self):
+        tk.messagebox.showinfo(
+            'About', 
+            message='Made by Leo Qi!'
+                    '\nVersion: ' + VERSION +
+                    '\nConnected to server? ' + str(self.email_app != None) +
+                    '\nGot emails?  ' + str(self.email_get != None)
+        )
+    
+    def connect(self):
+        if self.email_app == None:
+            self.put_msg('Connecting to server...')
+            self.email_app = EmailConnection()
+            self.put_msg('Connected!')
+        else:
+            self.log.put('Already connected!')
+    
+    def pre_get_mail(self):
+        if self.email_app is not None:
+            if self.email_get is not None:
+                self.put_msg('Getting messages')
+                TITLE = 'Get Messages'
+                threads = tk.simpledialog.askinteger(
+                    TITLE, 'Enter amount of threads for search (Min 1, Max 10)', 
+                    minvalue=1, maxvalue=10
+                )
+                if threads == None:
+                    self.put_msg('Cancelled.')
+                    return False
+            
+                self.tasks.append(Thread(target=self.get_mail, args=(threads,)))
+                self.tasks[-1].start()
+                return True
+            else:
+                self.put_msg('Emails already received.')
+        else:
+            self.put_msg('You must connect to the server first. Connecting...')
+            self.connect()
+            self.pre_refresh()
+
+    def get_mail(self, threads):
+        self.email_get = EmailGetter(self.email_app.conn, threads, self.put_msg)
+
+    def put_msg(self, msg):
+        """
+        Put a message into the queue to be displayed by the status bar
+
+        Keyword arguments:
+        msg -- A string displayed in the status bar
+        """
+        self.log.put(msg)
+
     def update_status(self):
+        """
+        Updates the status bar. Do not touch.
+        """   
         if not self.log.empty():
             self.status.set(self.log.get())
             self.log.task_done()
-        self.root.after(200, self.update_status)
-    
-    def connect_mail(self):
-        self.log.put('Connecting to server...')
-        self.EmailApp = EmailConnection()
-        self.log.put('Connected!')
-
-    def download_mail(self):
-        pass
-
-##    count = 0
-##    yes_count = 0
-##    no_count = 0
-##    searches = input('Enter comma-separated search terms: ').replace(' ', '').lower().split(',')
-##    print('searches')
-##    print('Searching for terms:')
-##    print('====================')
-##    for search in searches:
-##        print(search)
-##    print('====================')
-##    threads = 11
-##    while threads > 10:
-##        try:
-##            threads = int(input('Enter amount of threads (Max 10): '))
-##        except ValueError:
-##            pass
-##    start = time()
-##    email_get = EmailGetter(conn, threads)
-##    messages = email_get.emails
-##    if messages:
-##        copy_list = []
-##        subject = askForBool('Search subject lines?')
-##        to_ln = askForBool('Search To: lines?')
-##        from_ln = askForBool('Search From: lines?')
-##        process_message_searches = partial(
-##            process_message,
-##            subject=subject,
-##            to_ln=to_ln,
-##            from_ln=from_ln,
-##            search_list=searches
-##        )
-##        print('\nProcessing messages -> ', end='')
-##        with Pool() as pool:
-##             for i in pool.imap_unordered(process_message_searches, messages):
-##                count += 1
-##                if i != False:
-##                    copy_list.append(i)
-##                    print('O', end='')
-##                    yes_count += 1
-##                else:
-##                    print('X', end='')
-##                    no_count += 1
-##        try:
-##            percent = (yes_count/no_count) * 100
-##        except ZeroDivisionError:
-##            percent = 0
-##        end = time()
-##        print('\n\n********** Finished processing emails **********')
-##        print(f'Processed {count} messages in {end - start}s')
-##        print(f'{percent}% ({yes_count}/{yes_count + no_count}) of messages match')
-##        if len(copy_list) > 0:
-##            method = ''
-##            while method not in ('c', 'm', 'd'):
-##                method = input('Enter whether to copy, move or display: (C, M, D): ').strip().lower()
-##            print(f'Method: {method}')
-##            
-##            if method == 'c' or method == 'm':
-##                store_folder = input('Enter storage folder: ').strip()
-##                store_folder = "".join(store_folder.split())
-##                if method == 'c':
-##                    email_server.copy_emails(copy_list, store_folder)
-##                elif method == 'm':
-##                    email_server.move_emails(copy_list, store_folder)
-##            elif method == 'd':
-##                subjects = email_get.get_subjects(copy_list)
-##                print('\nSubject Lines:')
-##                print('====================')
-##                for subject in subjects:
-##                    print(subject)
-##                print('====================')
-##        else:
-##            print('\nNo messages fetched...')
-##
-##        print('\nFinished all operations for this thread')
-##        del email_get
-##        del email_server      
-##    else:
-##        print('No messages')
+        self.root.after(500, self.update_status)
 
 if __name__ == '__main__':
     app=Application()
